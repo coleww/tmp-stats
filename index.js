5b9706187191495a46cf6e74b1a7cf6a76807de2
Cole Willsea <coleww@gmail.com>
2014-08-30 06:28:20 -0700

    refactors all of the things

--- index.js
+++ index.js
@@ -4,36 +4,18 @@ var path = require('path');
 module.exports.getStat = getStat;
 module.exports.randomPath = randomPath;
 
-
 // helper buddy to get a randomPath for writing tmp files
 function randomPath () {
   return path.join(__dirname, Date.now() + '-' + Math.random().toString().substring(2));
 }
 
-
-
-// pass it a blob/stream/file/thing/dude/bro,
-// and a callback (err, stat, tmpPath)
-// it gets u the files stat obj
-// and then it destroys that file
-// gives u tmp path just in case?
-//is the enc necessary? how to make it optional?
-function getStat (blobStreamThingyDude, enc, cb) {
+// writes a stream to a file, gets the stats, and then deletes the file
+// for determining file size before upload and etc.
+function getStat (data, enc, cb) {
   var tmpFile = randomPath();
   var tmpWS = fs.createWriteStream(tmpFile);
-
-  // might be YAGNI ATM
   var tmpDeleted = false;
-  // hmmm....
-
-  tmpWS.write(blobStreamThingyDude, enc, function(data, end, err){//is that even correct for the args?
-    // do something with err if err lol
-    // end();
-
-    fs.stat(tmpFile, function(err, stat){
-      cb(err, stat);
-    });
-
+  var deleteTmp = function(tmpFile){
     if(!tmpDeleted){
       fs.unlink(tmpFile, function(err){
         if(err) {
@@ -41,17 +23,18 @@ function getStat (blobStreamThingyDude, enc, cb) {
         } else {
           tmpDeleted = true;
         }
-
-        // blab loudly if there is an error
       });
     }
-
+  };
+  tmpWS.write(data, enc, function(data, end, err){
+    if(err) {
+      cb(err);
+      deleteTmp(tmpFile);
+    }
+    fs.stat(tmpFile, function(err, stat){
+      cb(err, stat);
+      deleteTmp(tmpFile);
+    });
   });
   tmpWS.end();
-  // tmpWS.on('finish', function(){
-
-  // });
-  // tmpWS.on('error', function(err){
-  //     cb(err);
-  // });
 }
