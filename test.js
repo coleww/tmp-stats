ef4f55545acd0b3c048460e0c5c1e8039d7f1f71
Cole Willsea <coleww@gmail.com>
2014-08-30 06:26:38 -0700

    adds test that working dir does not getting larger or smaller...

--- test.js
+++ test.js
@@ -4,11 +4,13 @@ var test = require('tap').test;
 var tmpStream = require('./');
 
 test('tmp-stream', function(t) {
-    t.plan(3);
+    t.plan(4);
+    files = fs.readdirSync('/');
     tmpStream.getStat("hello world!", 'utf-8', function(err, stat){
       t.notOk(err, 'no err');
       t.ok(stat, 'lalalalalala');
       t.ok(stat.size === 12, '12 chars!');
+      t.ok(files.length === fs.readdirSync('/').length, 'cleans up after itself!');
       t.end();
     });
 });
